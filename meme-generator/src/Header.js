2529ffdb813235e7f909f7dc2b959995907cd124
JeffCord <jeffcord97@gmail.com>
2021-09-22 01:38:49 -0500

    first commit

---
+++ meme-generator/src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="https://i.kym-cdn.com/entries/icons/original/000/030/157/womanyellingcat.jpg"
+                alt="WomanYellingAtCat"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
